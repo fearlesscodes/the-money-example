4fb59954af96d027e13b72affca5972d4a43945c
Anton Truong <antont86@gmail.com>
2019-09-07 15:38:06 +0200

    Initial commit with spring boot starter package

---
+++ src/main/java/de/fearlessdeveloper/moneyexample/MoneyExampleApplication.java
@@ -0,0 +1,13 @@
+package de.fearlessdeveloper.moneyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fearlessdeveloper/moneyexample/MoneyExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package de.fearlessdeveloper.moneyexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneyExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
