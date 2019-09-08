a43a9ae2c1fe90e85d6837fc50626a907e09b433
Anton Truong <antont86@gmail.com>
2019-09-08 14:14:42 +0200

    Introduces the money example developed in tdd

---
+++ src/main/java/de/fearlessdeveloper/moneyexample/Dollar.java
@@ -0,0 +1,33 @@
+package de.fearlessdeveloper.moneyexample;
+
+import java.util.Objects;
+
+public class Dollar {
+    private int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Dollar times(int multiplier) {
+        amount = amount * multiplier;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
--- src/test/java/de/fearlessdeveloper/moneyexample/MoneyExampleApplicationTests.java
+++ src/test/java/de/fearlessdeveloper/moneyexample/MoneyExampleApplicationTests.java
@@ -5,12 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MoneyExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void dollarMulitplication() {
+		var dollar = new Dollar(5);
+		assertThat(dollar.getAmount(), is(5));
+		var dollarTimesFive = dollar.times(2);
+		assertThat(dollarTimesFive.getAmount(), is(10));
+
 	}
 
 }
